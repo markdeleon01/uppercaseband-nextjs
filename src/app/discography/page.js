c812a4aa67f0d6c49098d84142b4cc0f64b251b0
Mark de Leon <mark_deleon@hotmail.com>
2023-10-31 04:49:50 -0400

    Updated content for Discography page

--- src/app/discography/page.js
+++ src/app/discography/page.js
@@ -11,6 +11,8 @@ export default async function Discography() {
 			<h2 data-testid='discography-header' className={styles.discographyHeader}>
 				Discography
 			</h2>
+			<p className={styles.discographyHeadline}>The band released a full-length album, an EP and top-hit singles <br />that have garnered popularity and heavy streaming <br />from listeners in the Philippines and around the world.
+			</p>
 			{releases &&
 				releases.map((release) => (
 					<DiscographyListing key={release.title} release={release} />
