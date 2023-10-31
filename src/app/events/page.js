cddf51e45a8ce0a58f1670af05afd40dc66c929c
Mark de Leon <mark_deleon@hotmail.com>
2023-10-31 04:44:34 -0400

    Events page

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,7 +1,18 @@
-export default function Events() {
+import styles from './page.module.css'
+
+import { fetchEvents } from '../services/EventService'
+import EventListing from '../components/EventListing'
+
+export default async function Events() {
+	const events = await fetchEvents()
+
 	return (
-		<>
-			<h2>Events</h2>
-		</>
+		<div data-testid='events' className={styles.events}>
+			<h2 data-testid='events-header' className={styles.eventsHeader}>
+				Events
+			</h2>
+			{events &&
+				events.map((event) => <EventListing key={event.id} event={event} />)}
+		</div>
 	)
 }
